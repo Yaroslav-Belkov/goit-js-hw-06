30db17ed84305c018fc3d802d9b1caa579bbb4ef
Yaroslav-Belkov <yaroslav.belkov.91@gmail.com>
2022-12-10 23:55:04 +0200

    8 hw

--- js/task-08.js
+++ js/task-08.js
@@ -20,3 +20,12 @@
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде 
 // ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+const formEl = document.querySelector('.login-form')
+console.log(formEl)
+
+formEl.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit (event) {
+    event.preventDefault()
+    console.log ('Submit forms')
+}
